5cca0f4e3376ba5902e053257763e1638ba15967
Yehuda Lehrfield <yll2113@columbia.edu>
2023-10-04 23:00:42 -0300

    initial commit

---
+++ src/test/java/com/yl/reservation/host/HostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yl.reservation.host;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
